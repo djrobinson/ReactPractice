0fb15c97e45e3b91d7a05b86604159d82b65eac5
Danny Robinson <daniel@djrobinson.me>
2016-04-17 19:57:51 -0600

    Completed React router setup

--- imgur/src/app.jsx
+++ imgur/src/app.jsx
@@ -1,43 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var ReactRouter = require('react-router');
-var Router = ReactRouter.Router;
-var Route = ReactRouter.Route;
-
-var Hello = React.createClass({
-  render: function(){
-    return <div>
-      <h2>Hello!</h2>
-        {this.props.children}
-    </div>
-  }
-});
-
-var Child1 = React.createClass({
-  render: function(){
-    return <div>
-      <h2>I am child one</h2>
-      {this.props.children}
-    </div>
-  }
-});
-
-var Child2 = React.createClass({
-  render: function(){
-    return <div>
-      <h2>I am child two</h2>
-    </div>
-  }
-});
-
-var Routes = (
-    <Router>
-      <Route path="/" component={Hello}>
-        <Route path="1" component={Child1} />
-        <Route path="2" component={Child2} />
-        </Route>
-    </Router>
-  );
+var Routes = require('./routes');
 
 
 ReactDOM.render(Routes, document.querySelector('.container'));
