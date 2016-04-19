d40f08b9ea0c8611dd974da6ce5f308a7a13b457
Danny Robinson <daniel@djrobinson.me>
2016-04-19 15:18:08 -0600

    Added header links dynamically

--- imgur/src/components/topic-list.jsx
+++ imgur/src/components/topic-list.jsx
@@ -1,19 +1,21 @@
 var React = require('react');
+var Reflux = require('reflux');
 var TopicStore = require('../store/topic-store');
+var Actions = require('../actions');
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
 
 module.exports = React.createClass({
+  mixins: [
+    Reflux.listenTo(TopicStore, 'onChange')
+  ],
   getInitialState: function(){
     return {
       topics: []
     }
   },
   componentWillMount: function(){
-    TopicStore.getTopics()
-      .then(function(){
-        this.setState({
-          topics: TopicStore.topics
-        });
-      }.bind(this));
+    Actions.getTopics();
   },
   render: function(){
     return <div className="list-group">
@@ -22,10 +24,16 @@ module.exports = React.createClass({
     </div>
   },
   renderTopics: function(){
-    return this.state.topics.map(function(topic){
-      return <li key={topic.id}>
-        {topic.name}
-      </li>
+    return this.state.topics.slice(0,4).map(function(topic){
+      return <Link to={"topics/" + topic.id} key={topic.id} className="list-group-item">
+        <h4>{topic.name}</h4>
+        <p>{topic.description}</p>
+      </Link>
     });
+  },
+  onChange: function(event, topics){
+    this.setState({
+      topics: topics
+    })
   }
 })
